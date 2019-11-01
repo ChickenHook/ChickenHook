dcd8d13b63035cb585bccc091bc23003078e7748
Sascha Roth <sascha.roth@kobil.com>
2019-11-01 03:37:10 +0100

    added webview test

--- androidTest/src/androidTest/java/com/self/vmcracker/RegressionTest.kt
+++ androidTest/src/androidTest/java/com/self/vmcracker/RegressionTest.kt
@@ -1,6 +1,9 @@
 package com.self.vmcracker
 
+import android.os.SystemClock
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -24,5 +27,13 @@ class RegressionTest {
     @Test
     fun startMainActivity() {
         onView(withId(R.id.sample_text)).check(matches(withText("Hello from our Hook ;)")))
+    }
+
+    @Test
+    fun openWebView() {
+        onView(withId(R.id.test_button)).perform(click())
+        SystemClock.sleep(1000)
+        Espresso.pressBack();
+
     }
 }
