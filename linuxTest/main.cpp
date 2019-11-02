a5c88b1ffb1ba8cbd1625ce67f20bd77115b9e57
Sascha Roth <sascha.roth@kobil.com>
2019-11-02 06:13:25 +0100

    added first linux test

---
+++ linuxTest/main.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include "chickenHook/hooking.h"
+#include "chickenHook/trampoline.h"
+#include "chickenHook/logging.h"
+
+bool doIt() {
+    return true;
+}
+
+bool myDot() {
+    return false;
+}
+
+void logCallback(const std::string logtext) {
+    std::cout << logtext << std::endl;
+}
+
+int main(int argc, char *arv[]) {
+    std::cout << "WELCOME TO LINUX CHICKENHOOK TESTS" << std::endl;
+
+    // 1. set logging callback
+    ChickenHook::Hooking::getInstance().setLoggingCallback(&logCallback);
+
+    // 2. install a hook
+    ChickenHook::Hooking::getInstance().hook((void *) &doIt, (void *) &myDot);
+
+    // 3. check if hook works
+    if (doIt()) {
+        return -1;
+    }
+
+    return 0;
+}
